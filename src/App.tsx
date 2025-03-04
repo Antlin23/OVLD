7662ed730dabf4bfe9c95944ad72c9841e81b345
Antlin23 <anton.jumkil@gmail.com>
2025-03-04 01:20:29 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,92 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import skylines from './img/skylinesovld.png'
+import carsfrombeh from './img/highrescarsbeh.png'
+import skyline from './img/skyline.png'
+import openhoods from './img/openhoodcarsday.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <section className="carousel" aria-label="Gallery">
+
+  <ol className="carousel__viewport">
+
+    <li id="carousel__slide1"
+        className="carousel__slide">
+      <div className="carousel__snapper">
+        <a href="#carousel__slide4"
+           className="carousel__prev"><i className="fa-solid fa-arrow-left"></i></a>
+           
+           <div className='showcase'>
+            <div className='text'>
+            <h1 className='title'>OVERLOAD AUTO CLUB™</h1>
+            <div className='slogan'>- In speed we trust, in unity we thrive</div>
+            </div>
+            <img src={carsfrombeh}></img>
+          </div>
+
+        <a href="#carousel__slide2"
+           className="carousel__next"><i className="fa-solid fa-arrow-right"></i></a>
+      </div>
+    </li>
+
+    <li id="carousel__slide2"
+        className="carousel__slide">
+      <div className="carousel__snapper"></div>
+      <a href="#carousel__slide1"
+           className="carousel__prev"><i className="fa-solid fa-arrow-left"></i></a>
+
+          <div className='showcase'>
+            <div className='text'>
+            <h1 className='title'>OVERLOAD AUTO CLUB™</h1>
+            <div className='slogan'>- In speed we trust, in unity we thrive</div>
+            </div>
+            <img src={openhoods}></img>
+          </div>
+
+      <a href="#carousel__slide3"
+           className="carousel__next"><i className="fa-solid fa-arrow-right"></i></a>
+    </li>
+
+    <li id="carousel__slide3"
+        className="carousel__slide">
+      <div className="carousel__snapper"></div>
+      <a href="#carousel__slide2"
+           className="carousel__prev"><i className="fa-solid fa-arrow-left"></i></a>
+
+        <div className='showcase'>
+          <div className='text'>
+          <h1 className='title'>OVERLOAD AUTO CLUB™</h1>
+          <div className='slogan'>- In speed we trust, in unity we thrive</div>
+          </div>
+          <img src={skyline}></img>
+        </div>
+
+      <a href="#carousel__slide4"
+           className="carousel__next"><i className="fa-solid fa-arrow-right"></i></a>
+    </li>
+
+    <li id="carousel__slide4"
+        className="carousel__slide">
+      <div className="carousel__snapper"></div>
+      <a href="#carousel__slide3"
+           className="carousel__prev"><i className="fa-solid fa-arrow-left"></i></a>
+
+        <div className='showcase'>
+          <div className='text'>
+          <h1 className='title'>OVERLOAD AUTO CLUB™</h1>
+          <div className='slogan'>- In speed we trust, in unity we thrive</div>
+          </div>
+          <img src={skylines}></img>
+        </div>
+
+      <a href="#carousel__slide1"
+           className="carousel__next"><i className="fa-solid fa-arrow-right"></i></a>
+    </li>
+  </ol>
+</section>
+    </>
   );
 }
 
