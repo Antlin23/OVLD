7a020f786fa4a80ec86bf8f2bcf3e2aab3592802
Antlin23 <anton.jumkil@gmail.com>
2025-06-28 23:50:05 +0200

    updated img

--- src/StaffCarousel.tsx
+++ src/StaffCarousel.tsx
@@ -5,7 +5,7 @@ import img2 from './img/sandyshoredsairfield.jpg';
 import img3 from './img/highrescarsbeh.jpg';
 import img4 from './img/skyline.jpg';
 import img5 from './img/gangsters.jpg';
-import img6 from './img/skylinesnologo.jpg';
+import img6 from './img/phonefontain.jpg';
 
 const images = [img1, img2, img3, img4, img5, img6];
 
